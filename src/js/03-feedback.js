a89fc12001ed556ac022b19e500e4cdb65018f5b
Adrian Lacheta <adiczq@gmail.com>
2023-04-15 17:41:56 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formEl = document.querySelector('form');
+const info = {};
+
+fillForm();
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  info.email = formEl.elements.email.value;
+  info.message = formEl.elements.message.value;
+
+  console.log(info);
+
+  formEl.reset();
+  localStorage.removeItem('feedback-form-state');
+}
+
+function getInfo(e) {
+  info[e.target.name] = e.target.value;
+  localStorage.setItem('feedback-form-state', JSON.stringify(info));
+}
+
+function fillForm() {
+  const savedForm = localStorage.getItem('feedback-form-state');
+  if (savedForm) {
+    const setForm = JSON.parse(savedForm);
+    Object.keys(setForm).forEach(prop => {
+      formEl.elements[prop].value = setForm[prop];
+      info[prop] = setForm[prop];
+    });
+  }
+}
+
+formEl.addEventListener('submit', onFormSubmit);
+formEl.addEventListener('input', throttle(getInfo, 500));
